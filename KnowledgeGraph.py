79009d10f38aa89fc9e017ec4cdd35c3fc6d0c2a
akshitmanocha <akshitmanocha37@gmail.com>
2024-10-31 20:35:46 +0530

    Update KnowledgeGraph.py
    Added Hugginface Login

--- KnowledgeGraph.py
+++ KnowledgeGraph.py
@@ -3,38 +3,38 @@ from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
 from py2neo import Graph, Node, Relationship
-import pandas as pd
+from huggingface_hub import login
 
 load_dotenv()
 neo4j_uri = os.getenv("NEO4J_URI")
 neo4j_user = os.getenv("NEO4J_USERNAME")
 neo4j_password = os.getenv("NEO4J_PASSWORD")
+hf_token = os.getenv("HF_TOKEN") 
+
+
+login(token=hf_token)
 
 df = pd.read_json("hf://datasets/fhai50032/Symptoms_to_disease_7k/Symptoms_to_disease_7k.json")
 df['query'] = df['query'].apply(lambda x: x.split('Patient:I may have ', 1)[-1])
 df['response'] = df['response'].apply(lambda x: x.split('You may have ', 1)[-1])
 df['text'] = df.apply(lambda row: f'Symptoms: {row["query"]} Disease: {row["response"]}', axis=1)
-df.drop(columns=['query','response'],inplace=True)
+df.drop(columns=['query','response'], inplace=True)
 df = df[df['text'].apply(lambda x: len(x.split()) <= 50)]
 
 graph = Graph(neo4j_uri, auth=(neo4j_user, neo4j_password))
 
 for _, row in df.iterrows():
-# Extract symptoms and disease from the text
-text = row['text']
-symptoms_text = text.split("Symptoms: ")[1].split(" Disease: ")[0]
-disease_text = text.split(" Disease: ")[1]
-
-# Disease Node
-disease_node = Node("Disease", name=disease_text.strip())
-graph.merge(disease_node, "Disease", "name")
-
-# Create Symptom Nodes and relationships
-symptoms = symptoms_text.split(',' or '.')
-for symptom in symptoms:
-    symptom_node = Node("Symptom", name=symptom.strip())
-    graph.merge(symptom_node, "Symptom", "name")
+    text = row['text']
+    symptoms_text = text.split("Symptoms: ")[1].split(" Disease: ")[0]
+    disease_text = text.split(" Disease: ")[1]
+    disease_node = Node("Disease", name=disease_text.strip())
+    graph.merge(disease_node, "Disease", "name")
     
-    # Create a HAS_SYMPTOM relationship
-    relationship = Relationship(disease_node, "HAS_SYMPTOM", symptom_node)
-    graph.merge(relationship)
+    symptoms = symptoms_text.replace('.', ',').split(',')
+    for symptom in symptoms:
+        symptom = symptom.strip()
+        if symptom: 
+            symptom_node = Node("Symptom", name=symptom)
+            graph.merge(symptom_node, "Symptom", "name")
+            relationship = Relationship(disease_node, "HAS_SYMPTOM", symptom_node)
+            graph.merge(relationship)
